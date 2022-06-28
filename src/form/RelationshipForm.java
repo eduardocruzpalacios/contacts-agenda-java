ec32868264298ddec44a1236c4600cb1ed8f1958
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-06-28 22:36:02 +0200

    YAGNI

--- src/form/RelationshipForm.java
+++ src/form/RelationshipForm.java
@@ -1,10 +1,8 @@
 package form;
 
-import exceptions.RelationshipNotFoundException;
 import gui.Menu;
 import model.Relationship;
 import tools.In;
-import tools.Log;
 
 public class RelationshipForm {
 
@@ -14,20 +12,11 @@ public class RelationshipForm {
 		do {
 			Menu.relationship();
 			relationshipString = In.getString("Relationship:").toUpperCase();
-			try {
-				int count = 0;
-				for (Relationship relationship : Relationship.BY_ID.values()) {
-					count++;
-					if (relationship.toString().equals(relationshipString)) {
-						exit = true;
-						break;
-					}
-					if (count == Relationship.BY_ID.size()) {
-						throw new RelationshipNotFoundException();
-					}
+			for (Relationship relationship : Relationship.BY_ID.values()) {
+				if (relationship.toString().equals(relationshipString)) {
+					exit = true;
+					break;
 				}
-			} catch (RelationshipNotFoundException e) {
-				Log.logger.error(e + ": " + relationshipString);
 			}
 		} while (!exit);
 		return Relationship.valueOf(relationshipString);
