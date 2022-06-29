ecc185815135b5a5ce01a03d06a3d9ae8ef6e77a
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-06-29 20:43:54 +0200

    format

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -14,7 +14,6 @@ public class Menu {
 		Print.str("5. Exit");
 	}
 
-
 	public static void relationship() {
 		Print.header("RELATIONSHIP MENU");
 		for (Relationship relationship : Relationship.BY_NAME.values()) {
